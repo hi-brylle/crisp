21bb17265e137deedd0f8436fd53f5ba90ed4c52
hi-brylle <brylle.cayabyab.viernes@gmail.com>
2024-11-07 11:04:46 +0800

    Make program non-terminal non-silent

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,16 @@ fn main() {
     let src = read_to_string(args().nth(1).unwrap()).unwrap();
     
     let parse_result = GrammarParser::parse(Rule::Program, &src);
+    println!("{:?}\n", parse_result);
+
 
     match parse_result {
         Ok(mut pairs) => {
-            let root: pest::iterators::Pair<'_, Rule> = pairs.next().unwrap();
-            println!("{:?}", root);
+            // let root: pest::iterators::Pair<'_, Rule> = pairs.next().unwrap();
+            println!("{:?}\n", pairs.next().unwrap());
+            // println!("{:?}\n", pairs.next().unwrap());
+            // println!("{:?}\n", pairs.next().unwrap());
+            // println!("{:?}", root);
             // println!("{:?}", build_program_ast(root));
         },
         Err(e) => {
